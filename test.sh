0b60dfd190379c668625c52f6d220e6d02626156
John Pritchard <logicalexistentialism@gmail.com>
2022-09-10 01:18:45 -0400

    test

--- test.sh
+++ test.sh
@@ -1,10 +1,44 @@
 #!/bin/bash
 
-if twurl user get 'https://api.twitter.com/2/tweets/909818723028127749?expansions=author_id&tweet.fields=created_at&user.fields=username' print
+function test_created {
+    if twurl user get 'https://api.twitter.com/2/tweets/909818723028127749?expansions=author_id&tweet.fields=created_at&user.fields=username' print
+    then
+        echo 
+        return 0
+    else
+        echo ng
+        return 1
+    fi
+}
+
+function test_embeded {
+    
+    if reference=$(urlencode "https://twitter.com/US_CYBERCOM/status/1425129511520190470") &&
+       twurl user get "https://publish.twitter.com/oembed?url=${reference}&maxwidth=500&maxheight=500&omit_script=1" print
+    then
+        echo 
+        return 0
+    else
+        echo ng
+        return 1
+    fi
+}
+
+function test_together {
+    if reference=$(urlencode "https://twitter.com/US_CYBERCOM/status/1425129511520190470") &&
+       twurl  user get 'https://api.twitter.com/2/tweets/909818723028127749?expansions=author_id&tweet.fields=created_at&user.fields=username' print user get "https://publish.twitter.com/oembed?url=${reference}&maxwidth=500&maxheight=500&omit_script=1" print
+    then
+        echo 
+        return 0
+    else
+        echo ng
+        return 1
+    fi
+}
+
+if test_together
 then
-  echo 
-  exit 0
+    exit 0
 else
-  echo ng
-  exit 1
+    exit 1
 fi
