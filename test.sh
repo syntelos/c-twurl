ebd11496c892861c0938815369519bf6bf8bb273
John Pritchard <logicalexistentialism@gmail.com>
2022-09-09 22:30:44 -0400

    test

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if twurl user get 'https://api.twitter.com/2/tweets/909818723028127749?tweet.fields=created_at' print
+if twurl user get 'https://api.twitter.com/2/tweets/909818723028127749?expansions=author_id&tweet.fields=created_at&user.fields=username' print
 then
   echo 
   exit 0
